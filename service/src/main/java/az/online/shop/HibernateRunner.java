7d08ec59f2cc40e854395c01c6a5ac4dbb8a30cc
Anar Musayev <anarmusayev@digirella.az>
2022-10-11 22:08:20 +0400

    added HibernateRunner class

---
+++ service/src/main/java/az/online/shop/HibernateRunner.java
@@ -0,0 +1,13 @@
+package az.online.shop;
+
+import az.online.shop.entity.Customer;
+import az.online.shop.util.HibernateUtil;
+import javax.persistence.LockModeType;
+import org.hibernate.Session;
+
+public class HibernateRunner {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.buildSession();
+        session.find(Customer.class, 1, LockModeType.OPTIMISTIC);
+    }
+}
