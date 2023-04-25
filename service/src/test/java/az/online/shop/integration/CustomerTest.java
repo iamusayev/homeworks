70083e83d7f9db2a378b5d0348065714f2a8f467
Anar <95879636+iamusayev@users.noreply.github.com>
2023-04-25 23:36:47 +0400

    Criteria api implementation (#4)
    * added all classes
    
    * Update hibernate.cfg.xml
    
    * added database scripts
    
    * added CustomerDao and CustomerDaoIT classes (Criteria API Realization)
    
    * added create tables and add random data
    
    ---------
    
    Co-authored-by: Anar Musayev <anarmusayev@digirella.az>

---
+++ service/src/test/java/az/online/shop/integration/CustomerTest.java
@@ -0,0 +1,108 @@
+package az.online.shop.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.TestInstance.*;
+
+import az.online.shop.dao.CustomerDao;
+import az.online.shop.entity.Customer;
+import az.online.shop.entity.Order;
+import az.online.shop.model.Status;
+import az.online.shop.util.HibernateUtil;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Cleanup;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(Lifecycle.PER_CLASS)
+class CustomerTest {
+
+    private final SessionFactory sessionFactory = HibernateUtil.buildSession().getSessionFactory();
+    private final CustomerDao customerDao = CustomerDao.getInstance();
+
+
+    @BeforeAll
+    public void initDb() {
+        System.out.println("Инициализируем наши сущности.");
+    }
+
+    @AfterAll
+    public void finish() {
+        sessionFactory.close();
+    }
+
+
+    @Test
+    void findAll() {
+        @Cleanup var session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        List<Customer> result = customerDao.findAll(session);
+        assertThat(result).hasSize(10);
+
+        List<String> names = result.stream().map(Customer::getFirstName).toList();
+        assertThat(names)
+                .containsExactlyInAnyOrder("Isobelle", "Findlay", "Cleveland", "Herman", "Brandy", "Kirby", "Donald", "Holman", "Woolley", "Riya");
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    void findAllByFirstName() {
+        @Cleanup var session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        List<Customer> results = customerDao.findAllByName(session, "Isobelle");
+
+        assertThat(results).hasSize(1);
+        assertThat(results.get(0).getFirstName()).isEqualTo("Isobelle");
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    void findLimitedCustomerOrderedByBirthday() {
+        @Cleanup var session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        int limit = 3;
+        List<Customer> results = customerDao.findLimitedCustomerOrderedByBirthday(session, limit);
+        assertThat(results).hasSize(3);
+
+        List<String> names = results.stream().map(Customer::getFirstName).toList();
+        assertThat(names).contains("Holman", "Kirby", "Woolley");
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    void findAllByOrderStatus() {
+        @Cleanup var session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        List<Customer> results = customerDao.findAllByStatus(session, Status.ACTIVE);
+        assertThat(results).hasSize(9);
+
+        List<String> names = results.stream().map(Customer::getFirstName).toList();
+        assertThat(names).containsExactlyInAnyOrder("Findlay", "Cleveland", "Herman", "Brandy", "Kirby", "Donald", "Holman", "Woolley", "Riya");
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    void findAllOrdersByCustomerName() {
+        @Cleanup var session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        List<Order> results = customerDao.findAllOrdersByCustomerName(session, "Findlay");
+        assertThat(results).hasSize(1);
+
+        List<LocalDate> registrationDates = results.stream().map(Order::getRegistrationDate).toList();
+        assertThat(registrationDates.get(0)).isEqualTo(LocalDate.of(2020, 1, 3));
+
+        session.getTransaction().commit();
+    }
+}
